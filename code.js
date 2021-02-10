7c4d401094d504163401d89ee3ff04e37a71b3a3
Hunter Gault <hgault@software.private>
2021-02-10 13:15:19 -0500

    Added new files

---
+++ code.js
@@ -0,0 +1,24 @@
+var word = "bottles";
+		
+var count = 99;
+		
+	while (count > 0) {
+			
+		console.log(count + " " + word + " of water on the wall.");
+			
+		console.log(count + " " + word + " of water,");
+			
+		console.log("Take one down, pass it around,");
+			
+		count = count - 1;
+			
+	if (count > 0) {
+				
+		console.log(count + " " + word + " of water on the wall.");
+	} else {
+				
+		console.log("No more " + word + " of water on the wall.");
+				
+		}
+	}
+		
